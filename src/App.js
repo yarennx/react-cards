ea49d3bc6d2c43cb985f6cd5b1b8d7db948fc17d
yarenelemenci <yarenelemenci@gmail.com>
2023-06-05 18:00:34 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./App.css";
+import Card from "./Card.js";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <Card
+        img="https://i.pinimg.com/originals/71/bd/56/71bd568789fc02e553c217ee6c265d90.jpg"
+        title="Bloom🔥"
+        description="Ejder Ateşi Perisi"
+      />
+      <Card
+        img="https://i.pinimg.com/originals/87/2d/98/872d98cfb9862aefce2c23c989e0dc74.png"
+        title="Stella✨ "
+        description="Parlak Güneş Perisi"
+      />
+      <Card
+        img="https://i.pinimg.com/originals/cc/e7/f0/cce7f0a45af5e2860500f6c9042e3b4a.png"
+        title="Flora🌿"
+        description="Doğa Perisi"
+      />
+      <Card
+        img="https://img.wattpad.com/fff9fb824fa335df1fd82f32ef694d23829197e5/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f5268777253754b7844417a3762413d3d2d3538373730393733302e313533373231303462366464646261383939393037343534373934372e6a7067"
+        title="Miusa🎶"
+        description="Müzik Perisi"
+      />
+      <Card
+        img="https://i.pinimg.com/originals/72/9e/c3/729ec3f1da6b269a1f406427962728d2.png"
+        title="Tecna👾"
+        description="Teknoloji Perisi"
+      />
+      <Card
+        img="https://i.pinimg.com/originals/40/89/05/40890527c4fd05d6cb552e9cc429692e.jpg"
+        title="Aisha (Layla) 🌊"
+        description="Dalgalar Perisi"
+      />
+    </div>
+  );
+}
+
+export default App;
